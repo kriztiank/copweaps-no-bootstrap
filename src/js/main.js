d545bd6ae3ccf7359386b4814d075c794a9a8ad9
kriztiank <kriztiank@gmail.com>
2019-09-25 07:22:31 +0200

    modal added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
-import message from './message';
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
 
-message('Hello World!');
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Listen for click
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
