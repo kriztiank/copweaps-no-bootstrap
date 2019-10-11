ee367a93c9f3fcadc1bca098b160772247a9fcb3
kriztiank <kriztiank@gmail.com>
2019-10-11 12:23:16 +0200

    modal done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,7 @@
-// Get DOM Elements
-const modal = document.querySelector('#my-modal');
-const modalBtn = document.querySelector('#modal-btn');
-const closeBtn = document.querySelector('.close');
+document.getElementById('modal_btn').addEventListener('click', function() {
+  document.querySelector('.modal-bg').style.display = 'flex';
+});
 
-// Open
-function openModal() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-// Listen for click
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
+document.querySelector('.close').addEventListener('click', function() {
+  document.querySelector('.modal-bg').style.display = 'none';
+});
